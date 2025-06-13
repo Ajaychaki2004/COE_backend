da10583f15d364e11ed1d093d5156f844c9bb1c8
AjayChaki2004 <sajaychakravarthi2004@gmail.com>
2025-06-13 10:10:32 +0530

    ofx:CORS error fixed

--- backend/settings.py
+++ backend/settings.py
@@ -57,6 +57,15 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # React frontend
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_HEADERS = [
+    "Content-Type",
+    "Authorization",
+    "accept",
+    "X-CSRFToken",
+    "X-Requested-With",
 ]
 
 ROOT_URLCONF = 'backend.urls'
